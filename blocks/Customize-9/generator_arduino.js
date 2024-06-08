7e644a218cf0e9b2e2640b04c25860d739ecdd78
Xinyue Cao <ashleyue@students.cs.ubc.ca>
2024-06-07 18:45:37 -0700

    changed set_servo_angle_speed
    added an helper class so that currentAngle should persist
    
    TODO: test if actually work

--- blocks/Customize-9/generator_arduino.js
+++ blocks/Customize-9/generator_arduino.js
@@ -15,7 +15,7 @@ goog.require("Blockly.Arduino");
 
 var dropdown_pin = 9; // for now 
 
-var currentAngle = 0;
+var currentAngle = 90;
 
 
 // angle + speed block
@@ -25,24 +25,30 @@ Blockly.Arduino["set_servo_angle_speed"] = function (block) {
   var speed = Blockly.Arduino.valueToCode(block, 'SPEED', Blockly.Arduino.ORDER_ATOMIC);
 
   Blockly.Arduino.includes_["includes_servo"] = "#include <Servo.h>";
-  Blockly.Arduino.definitions_["var_servo" + dropdown_pin] =
-    "Servo servo_" + dropdown_pin + ";";
-  Blockly.Arduino.setups_["setup_servo_" + dropdown_pin] =
-    "servo_" + dropdown_pin + ".attach(" + dropdown_pin + ");";
-
-  Blockly.Arduino.addFunction('setServoPosition', ` void setServoPosition(int angle, int speed) {
-  int increment = (angle > ` + currentAngle + `) ? 1 : -1;
-  int delayTime = 1000 / speed; 
-
-  for (int pos = ` + currentAngle + `; pos != angle; pos += increment) {
-    servo_` + dropdown_pin + `.write(pos);
-    delay(delayTime);
-  }
-  ` + currentAngle + ` = angle; // Update the current angle to the target angle}`);
-
-  var code = 'setServoPosition(' + angle + ', ' + speed + ');\n';
+  Blockly.Arduino.definitions_["var_servo" + dropdown_pin] = "Servo servo_" + dropdown_pin + ";";
+  Blockly.Arduino.addFunction('ServoHelper_setServoAngleSpeed', `
+      class ServoHelper {
+      public:
+        static int currentAngle;
+        static void setServoAngleSpeed(int targetAngle, int speed) {
+          int increment = (targetAngle > currentAngle) ? 1 : -1;
+          int delayTime = 3000/speed; 
+          for (int pos = currentAngle; pos != angle; pos += increment) {
+              servo_` + dropdown_pin + `.write(pos);
+              delay(delayTime);
+          }
+          currentAngle = targetAngle; // Update the current angle to the target angle
+        }
+      };
+      int ServoHelper::currentAngle = 90;`);
+  
+  var code = 'ServoHelper::setServoAngleSpeed(' + angle + ', ' + speed + ');\n';
   return code;
 };
+
+  
+  
+
 
 
 
