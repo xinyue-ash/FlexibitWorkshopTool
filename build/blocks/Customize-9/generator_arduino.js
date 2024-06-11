5c73da5d9dcd4b1e56540d631e9b41cd1de48244
Xinyue Cao <ashleyue@students.cs.ubc.ca>
2024-06-11 16:29:02 -0700

    updated angle_speed block

--- build/blocks/Customize-9/generator_arduino.js
+++ build/blocks/Customize-9/generator_arduino.js
@@ -15,7 +15,7 @@ goog.require("Blockly.Arduino");
 
 var dropdown_pin = 9; // for now 
 
-var currentAngle = 90;
+var currentAngle = 0;
 
 
 // angle + speed block
@@ -26,25 +26,40 @@ Blockly.Arduino["set_servo_angle_speed"] = function (block) {
 
   Blockly.Arduino.includes_["includes_servo"] = "#include <Servo.h>";
   Blockly.Arduino.definitions_["var_servo" + dropdown_pin] = "Servo servo_" + dropdown_pin + ";";
+  Blockly.Arduino.setups_["setup_servo_" + dropdown_pin] = "servo_" + dropdown_pin + ".attach(" + dropdown_pin + ");";
+
   Blockly.Arduino.addFunction('ServoHelper_setServoAngleSpeed', `
       class ServoHelper {
       public:
         static int currentAngle;
         static void setServoAngleSpeed(int targetAngle, int speed) {
           int increment = (targetAngle > currentAngle) ? 1 : -1;
-          int delayTime = 3000/speed; 
-          for (int pos = currentAngle; pos != angle; pos += increment) {
+          int delayTime = 100/speed; 
+          for (int pos = currentAngle; pos != targetAngle; pos += increment) {
               servo_` + dropdown_pin + `.write(pos);
               delay(delayTime);
           }
           currentAngle = targetAngle; // Update the current angle to the target angle
         }
       };
-      int ServoHelper::currentAngle = 90;`);
+      int ServoHelper::currentAngle = 0;`);
   
   var code = 'ServoHelper::setServoAngleSpeed(' + angle + ', ' + speed + ');\n';
   return code;
 };
+
+
+// to be continued 
+Blockly.Arduino['multi_servo_control'] = function (block) {
+  var servo1_statements = Blockly.Arduino.statementToCode(block, 'FB1');
+  var servo2_statements = Blockly.Arduino.statementToCode(block, 'FB2');
+  var servo3_statements = Blockly.Arduino.statementToCode(block, 'FB3');
+
+
+  var code = servo1_statements + servo2_statements + servo3_statements;
+  return code;
+};
+
 
   
   
